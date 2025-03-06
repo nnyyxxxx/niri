0e0b75e8fd59c0217f25f089728532fa8f6961ed
nnyyxxxx <nnyyxxxx@protonmail.com>
2025-03-06 17:38:09 -0500

    refactor: improve quality

--- src/input/pick_color_grab.rs
+++ src/input/pick_color_grab.rs
@@ -37,6 +37,50 @@ impl PickColorGrab {
             .set_cursor_image(CursorImageStatus::default_named());
         state.niri.queue_redraw_all();
     }
+
+    fn pick_color_at_point(location: Point<f64, Logical>, data: &mut State) -> Option<[u8; 4]> {
+        let (output, pos_within_output) = data.niri.output_under(location)?;
+
+        data.backend
+            .with_primary_renderer(|renderer| {
+                let scale = Scale::from(output.current_scale().fractional_scale());
+                let physical_pos: Point<i32, Physical> =
+                    pos_within_output.to_physical_precise_round(scale);
+                let size = smithay::utils::Size::<i32, Physical>::from((1, 1));
+
+                let elements = data.niri.render::<GlesRenderer>(
+                    renderer,
+                    output,
+                    false,
+                    crate::render_helpers::RenderTarget::ScreenCapture,
+                );
+
+                let pixels = match render_to_vec(
+                    renderer,
+                    size,
+                    scale,
+                    output.current_transform(),
+                    Fourcc::Abgr8888,
+                    elements.iter().rev().map(|elem| {
+                        RelocateRenderElement::from_element(
+                            elem,
+                            Point::from((-physical_pos.x, -physical_pos.y)),
+                            Relocate::Relative,
+                        )
+                    }),
+                ) {
+                    Ok(pixels) => pixels,
+                    Err(_) => return None,
+                };
+
+                if pixels.len() == 4 {
+                    Some([pixels[0], pixels[1], pixels[2], pixels[3]])
+                } else {
+                    None
+                }
+            })
+            .flatten()
+    }
 }
 
 impl PointerGrab<State> for PickColorGrab {
@@ -66,67 +110,22 @@ impl PointerGrab<State> for PickColorGrab {
         handle: &mut PointerInnerHandle<'_, State>,
         event: &ButtonEvent,
     ) {
-        if event.state == ButtonState::Pressed {
-            self.temporary_location = Some(handle.current_location());
-
-            let current_location = self.temporary_location;
-
-            let color = if let Some(location) = current_location {
-                let output_info = data.niri.output_under(location);
-
-                if let Some((output, pos_within_output)) = output_info {
-                    data.backend
-                        .with_primary_renderer(|renderer| {
-                            let scale = Scale::from(output.current_scale().fractional_scale());
-                            let physical_pos: Point<i32, Physical> =
-                                pos_within_output.to_physical_precise_round(scale);
-                            let size = smithay::utils::Size::<i32, Physical>::from((1, 1));
-
-                            let elements = data.niri.render::<GlesRenderer>(
-                                renderer,
-                                output,
-                                false,
-                                crate::render_helpers::RenderTarget::ScreenCapture,
-                            );
-
-                            let pixels = match render_to_vec(
-                                renderer,
-                                size,
-                                scale,
-                                output.current_transform(),
-                                Fourcc::Abgr8888,
-                                elements.iter().rev().map(|elem| {
-                                    RelocateRenderElement::from_element(
-                                        elem,
-                                        Point::from((-physical_pos.x, -physical_pos.y)),
-                                        Relocate::Relative,
-                                    )
-                                }),
-                            ) {
-                                Ok(pixels) => pixels,
-                                Err(_) => return None,
-                            };
-
-                            if pixels.len() == 4 {
-                                Some([pixels[0], pixels[1], pixels[2], pixels[3]])
-                            } else {
-                                None
-                            }
-                        })
-                        .flatten()
-                } else {
-                    None
-                }
-            } else {
-                None
-            };
-
-            if let Some(tx) = data.niri.pick_color.take() {
-                let _ = tx.send_blocking(color);
-            }
-
-            handle.unset_grab(self, data, event.serial, event.time, true);
+        if event.state != ButtonState::Pressed {
+            return;
         }
+
+        self.temporary_location = Some(handle.current_location());
+
+        let color = match self.temporary_location {
+            Some(location) => Self::pick_color_at_point(location, data),
+            None => None,
+        };
+
+        if let Some(tx) = data.niri.pick_color.take() {
+            let _ = tx.send_blocking(color);
+        }
+
+        handle.unset_grab(self, data, event.serial, event.time, true);
     }
 
     fn axis(
